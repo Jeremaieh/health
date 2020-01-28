e0048313d9070e92473ba55b599308a51b0bb125
xiaoshiguang123 <xyt123@163.com>
2020-01-29 02:12:09 +0800

    :tada: init project

---
+++ pages/index/index.vue
@@ -0,0 +1,26 @@
+<template>
+	<view>
+		<text>首页</text>
+		<navigator url="../login/login">登录</navigator>
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				
+			}
+		},
+		onLoad() {
+
+		},
+		methods: {
+
+		}
+	}
+</script>
+
+<style lang="scss">
+	
+</style>
---
+++ pages/login/login.vue
@@ -0,0 +1,19 @@
+<template>
+	<view>
+		<text>登录页面</text>
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				
+			};
+		}
+	}
+</script>
+
+<style lang="scss">
+
+</style>
